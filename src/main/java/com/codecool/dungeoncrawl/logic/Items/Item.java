541d76244942b8c88f87c382a13edf42b1c3ee01
Fraktalia <g.w.koscielniak@gmail.com>
2021-03-23 11:04:40 +0100

    Add sword class.

--- src/main/java/com/codecool/dungeoncrawl/logic/Items/Item.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/Items/Item.java
@@ -6,4 +6,39 @@ public class Item {
     private String itemName;
     private int itemDamage;
     private int itemDefense;
+
+    public Item(Cell cell, String itemName, int itemDamage, int itemDefense) {
+        this.cell = cell;
+        this.itemName = itemName;
+        this.itemDamage = itemDamage;
+        this.itemDefense = itemDefense;
+    }
+
+    public Cell getCell(){
+        return cell;
+    }
+
+    public String getItemName(){
+        return itemName;
+    }
+
+    public int getItemDamage(){
+        return itemDamage;
+    }
+
+    public int getItemDefense(){
+        return itemDefense;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public void setItemDamage(int itemDamage) {
+        this.itemDamage = itemDamage;
+    }
+
+    public void setItemDefense(int itemDefense) {
+        this.itemDefense = itemDefense;
+    }
 }
---
+++ src/main/java/com/codecool/dungeoncrawl/logic/Items/Sword.java
@@ -0,0 +1,11 @@
+package com.codecool.dungeoncrawl.logic.Items;
+
+import com.codecool.dungeoncrawl.logic.Cell;
+
+public class Sword extends Item{
+    public Sword(Cell cell, String itemName, int itemDamage, int itemDefense) {
+        super(cell, itemName, itemDamage, itemDefense);
+    }
+
+    
+}
