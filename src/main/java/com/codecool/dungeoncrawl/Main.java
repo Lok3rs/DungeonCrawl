1cf77dd0d459d2668e24fb503996abb77ac9ce35
Dawid Adamski <lok3rsss@gmail.com>
2021-03-23 21:37:00 +0100

    Add labels for armor and attack in ui pannel

--- src/main/java/com/codecool/dungeoncrawl/Main.java
+++ src/main/java/com/codecool/dungeoncrawl/Main.java
@@ -22,6 +22,8 @@ public class Main extends Application {
             map.getHeight() * Tiles.TILE_WIDTH);
     GraphicsContext context = canvas.getGraphicsContext2D();
     Label healthLabel = new Label();
+    Label attackLabel = new Label();
+    Label armorLabel = new Label();
 
     public static void main(String[] args) {
         launch(args);
@@ -35,6 +37,10 @@ public class Main extends Application {
 
         ui.add(new Label("Health: "), 0, 0);
         ui.add(healthLabel, 1, 0);
+        ui.add(new Label("Attack: "), 0, 1);
+        ui.add(attackLabel, 1, 1);
+        ui.add(new Label("Armor: "), 0, 2);
+        ui.add(armorLabel, 1, 2);
 
         BorderPane borderPane = new BorderPane();
 
@@ -88,5 +94,7 @@ public class Main extends Application {
             }
         }
         healthLabel.setText("" + map.getPlayer().getHealth());
+        attackLabel.setText("" + map.getPlayer().getAttack());
+        armorLabel.setText("" + map.getPlayer().getArmor());
     }
 }
