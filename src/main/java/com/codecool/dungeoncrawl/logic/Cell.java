158c4fb935004c39aef1799f9c3676973495dfb5
Dawid Adamski <lok3rsss@gmail.com>
2021-03-25 19:54:54 +0100

    Mark some variables in Cell class as final

--- src/main/java/com/codecool/dungeoncrawl/logic/Cell.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/Cell.java
@@ -10,8 +10,8 @@ public class Cell implements Drawable {
     private CellType type;
     private Actor actor;
     private Item item;
-    private GameMap gameMap;
-    private int x, y;
+    private final GameMap gameMap;
+    private final int x, y;
 
     Cell(GameMap gameMap, int x, int y, CellType type) {
         this.gameMap = gameMap;
