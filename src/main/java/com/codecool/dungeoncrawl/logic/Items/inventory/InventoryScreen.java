cc25a7d8f6528f38c1202a8cf2e264c585714e32
Fraktalia <g.w.koscielniak@gmail.com>
2021-04-06 13:09:12 +0200

    Improve inventory styling

--- src/main/java/com/codecool/dungeoncrawl/logic/Items/inventory/InventoryScreen.java
+++ src/main/java/com/codecool/dungeoncrawl/logic/Items/inventory/InventoryScreen.java
@@ -17,13 +17,17 @@ public class InventoryScreen {
 
     public void drawInventory(Canvas canvas) {
         GraphicsContext context = canvas.getGraphicsContext2D();
-        context.setFill(Color.AQUAMARINE);
-        context.fillRect(0, 0, canvas.getWidth(), canvas.getHeight());
-//        Tiles.drawBigTile(context, "potion", 1, 5);
-//        Tiles.drawBigTile(context, "sword", 1, 3);
+
+        context.setFill(Color.BLACK);
+        context.fillRect(0, 0,
+                canvas.getWidth(), canvas.getHeight());
+
+        context.setFill(Color.web("#472D3C"));
+        context.fillRect(Tiles.TILE_WIDTH, Tiles.TILE_WIDTH,
+                canvas.getWidth()-2*Tiles.TILE_WIDTH, canvas.getHeight()-2*Tiles.TILE_WIDTH);
+
         for (int i = 0; i < inventory.size(); i++) {
-            Tiles.drawTile(context, inventory.get(i), 1, i*2+2, 2);
+            Tiles.drawTile(context, inventory.get(i), 1, i*2+1, 2);
         }
-//        Tiles.drawTile(context, brzeszczot, 1, 7, 2);
     }
 }
