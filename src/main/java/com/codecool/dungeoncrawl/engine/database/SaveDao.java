9c90930eaf9b14edf82281c9a50711f23d4b03bb
Dawid Adamski <lok3rsss@gmail.com>
2021-04-14 19:49:25 +0200

    Create DAO interface for save

---
+++ src/main/java/com/codecool/dungeoncrawl/engine/database/SaveDao.java
@@ -0,0 +1,16 @@
+package com.codecool.dungeoncrawl.engine.database;
+
+import com.codecool.dungeoncrawl.engine.map.GameMap;
+import com.codecool.dungeoncrawl.logic.Items.Item;
+import com.codecool.dungeoncrawl.logic.actors.Player;
+import com.codecool.dungeoncrawl.logic.actors.monsters.Monster;
+
+import java.util.List;
+
+public interface SaveDao {
+    List<Monster> getMonsters(GameMap map);
+    Player getPlayer(GameMap map);
+    Integer getMapNumber();
+    List<Item> getMapItems(GameMap map);
+    List<Item> getInventory(Player player);
+}
